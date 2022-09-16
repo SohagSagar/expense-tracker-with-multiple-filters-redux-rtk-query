709641700eabb7a9c9a97c6fd53caab50def685c
SohagSagar <sagorsohag29@gmail.com>
2022-09-17 00:50:54 +0600

    title changed

--- src/Components/TransactionsHeader.js
+++ src/Components/TransactionsHeader.js
@@ -58,7 +58,7 @@ const TransactionsHeader = ({ setIsAddModalOpen, currentPage, totalPage, setCurr
                 </div>
 
                 <div className="btn-group ">
-                    <button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)} className="btn xs:btn-sm lg:lg:btn-md xs:w-3">«</button>
+                    <button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)} className="btn xs:btn-sm lg:btn-md xs:w-3">«</button>
                     <button className="btn lg:btn-md xs:btn-sm xs:w-10 w-20 normal-case lg:text-md xs:text-[12px]">Page {currentPage}/{totalPage || 1}</button>
                     <button disabled={currentPage === totalPage} onClick={() => setCurrentPage(currentPage + 1)} className="btn xs:btn-sm lg:btn-md xs:w-3">»</button>
                 </div>
